37c52e52a44bdf77c5263583ad51fcd4046c2143
Gilbert <gssenyonjo@gmail.com>
2022-04-28 19:35:13 +0300

    Added spaces

--- app/src/main/java/net/passioncloud/democompose/MainActivity.kt
+++ app/src/main/java/net/passioncloud/democompose/MainActivity.kt
@@ -3,13 +3,19 @@ package net.passioncloud.democompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.passioncloud.democompose.ui.theme.DemoComposeTheme
 
 
@@ -24,15 +30,40 @@ class MainActivity: ComponentActivity(){
     }
 }
 
+
+data class Message(
+    val writer: String,
+    val body: String
+)
+
 @Preview
 @Composable
 fun PreviewMessenger() {
-    Messenger(info = "what did you eat.")
+    Messenger(msg=Message("Rooney", "Scored hat-trick"))
 }
 
+
+
 @Composable
-fun Messenger(info: String) {
-    Text(text = "Your info is: $info")
+fun Messenger(msg: Message) {
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.paula),
+            contentDescription = "Paula profile image",
+            modifier = Modifier
+                .width(50.dp)
+                .height(50.dp)
+                .clip(CircleShape)
+        )
+        // horizontal space
+        Spacer(modifier = Modifier.width(18.dp))
+        Column {
+            Text(text = msg.writer)
+            Spacer(modifier=Modifier.height(8.dp))
+            Text(text = msg.body)
+        }
+    }
+
 }
 
 
